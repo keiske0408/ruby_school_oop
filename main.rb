f2484aaf814c315592673935bd806d52cfbfdce8
keiske0408 <kieske0408@gmail.com>
2024-09-19 12:57:20 +0800

    adding new student

--- main.rb
+++ main.rb
@@ -1,1 +1,25 @@
-puts "Hello World"
+require './student.rb'
+
+
+# id, name, birth_date, :email, :phone_number
+puts "======Add new Student======"
+new_students = Student.new
+print "name: "
+new_students.name = gets.chomp
+print "birth_date: "
+new_students.birth_date = gets.chomp
+print "email: "
+new_students.email = gets.chomp
+print "phone_number: "
+new_students.phone_number = gets.chomp
+
+id = Student.all.size + 1
+new_students.id = id
+
+new_students.save
+new_students.display
+puts"Student added successfully!"
+
+
+
+
