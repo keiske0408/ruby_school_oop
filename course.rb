fc1f1f2499abd3f352e4a907eaf332712cf2929b
keiske0408 <kieske0408@gmail.com>
2024-09-19 16:14:52 +0800

    adding and deleting course

--- course.rb
+++ course.rb
@@ -1,24 +1,25 @@
 class Course
   attr_accessor :id, :name
-  @@records
-end
+  @@records = []
 
-def save
-  @@records << self
-end
+  def save
+    @@records << self
+  end
 
-def destroy
-  @@records.delete(self)
-end
+  def destroy
+    @@records.delete(self)
+  end
 
-def display
-  "\n course id: #{id}, name: #{name} \n"
-end
+  def display
+    "\nCourse ID: #{id},Course Name: #{name} \n"
+  end
 
-def self.all
-  return @@records
-end
+  def self.all
+    return @@records
+  end
 
-def self.find_id(id)
-  @@records.find{|course_id| course_id == id}
+  def self.find_id(id)
+    @@records.find { |course_id| course_id == id }
+  end
+
 end
--- main.rb
+++ main.rb
@@ -3,24 +3,23 @@ require './student.rb'
 
 def add_student
 
-# id, name, birth_date, :email, :phone_number
-puts "•      Add new Student      •"
-new_students = Student.new
-print "name: "
-new_students.name = gets.chomp
-print "birth_date: "
-new_students.birth_date = gets.chomp
-print "email: "
-new_students.email = gets.chomp
-print "phone_number: "
-new_students.phone_number = gets.chomp
+  puts "•      Add new Student      •"
+  new_students = Student.new
+  print "name: "
+  new_students.name = gets.chomp
+  print "birth_date: "
+  new_students.birth_date = gets.chomp
+  print "email: "
+  new_students.email = gets.chomp
+  print "phone_number: "
+  new_students.phone_number = gets.chomp
 
-id = Student.all.size + 1
-new_students.id = id
+  id = Student.all.size + 1
+  new_students.id = id
 
-new_students.save
-puts"Student added successfully!"
-puts new_students.display
+  new_students.save
+  puts "Student added successfully!"
+  puts new_students.display
 end
 
 def delete_student
@@ -37,29 +36,87 @@ def delete_student
   end
 end
 
+def student_management
+  puts "===========OPTIONS=========="
+  print "Add/Delete:"
+  option = gets.chomp.downcase
+  if option == "add"
+    add_student
+  else
+    if option == "delete"
+      delete_student
+    end
+  end
+  puts "invalid option"
+end
+
+def add_course
+  puts "•      Add new Course      •"
+  new_course = Course.new
+  print "Course Name: "
+  new_course.name = gets.chomp
+
+  id = Course.all.size + 1
+  new_course.id = id
+
+  new_course.save
+  puts "Course added successfully!"
+  puts new_course.display
+end
+
+def delete_course
+  puts "Input Course ID to delete:"
+  course_id = gets.chomp.to_i
+  course = Course.find_id(course_id)
+
+  if course
+    course.destroy
+    puts "Course successfully deleted!"
+  else
+    puts "Course ID not found"
+
+  end
+end
+
+def course_management
+
+  puts "===========OPTIONS=========="
+  print "Add/Delete:"
+  option = gets.chomp.downcase
+  if option == "add"
+    add_course
+  elsif option == "delete"
+    delete_course
+  else
+    puts "invalid option"
+    course_management
+  end
+end
+
+
 
 while true
-puts '✦---------------------------✦'
-puts '===========OPTIONS=========='
-puts '1. Add student'
-puts '2. Delete Student'
-puts '3. Exit'
-puts '✦---------------------------✦'
+  puts "\n✦---------------------------✦"
+  puts "\n===========OPTIONS==========\n"
+  puts '1. Student management'
+  puts '2. Course management'
+  puts '3. Exit'
+  puts "\n✦---------------------------✦\n"
 
-print 'Pick option: '
-option = gets.to_i
+  print 'Pick option: '
+  option = gets.to_i
 
-case option
-when 1
-  add_student
-when 2
-  delete_student
-when 3
-  puts "Exiting the program"
-  break
-else
-  puts "invalid option. Please try again."
-end
+  case option
+  when 1
+    student_management
+  when 2
+    course_management
+  when 3
+    puts "Exiting the program"
+    break
+  else
+    puts "invalid option. Please try again."
+  end
 end
 
 
